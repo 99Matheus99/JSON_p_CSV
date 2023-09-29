4fddd6d29691de0ac3c80e052b325374c40e7608
matheus de oliveira <nubinhopro5@gmail.com>
2023-09-29 20:27:20 -0300

    abrindo o arquivo csv e colocando os dados nele

--- conversor_JSON_CSV.py
+++ conversor_JSON_CSV.py
@@ -5,4 +5,7 @@ with open('dados.json', mode ='r', encoding='utf-8') as arquivo_json:
     leitor_json = json.load(arquivo_json)
     for dict in leitor_json:
         itens.append(list(dict.values())) # aqui eu guardo todos os valores do dicionário em uma lista
-    print(itens)
+with open('dados.csv', mode='w', encoding='utf-8', newline='') as arquivo_csv: # O newline ignora pulo de linhas
+    escritor_csv = csv.writer(arquivo_csv)
+    for linha in itens:
+        escritor_csv.writerow(linha)
